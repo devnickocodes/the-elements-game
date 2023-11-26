3ee815bd1409679ce412b95ae509daec494a487f
devnickocodes <>
2023-11-26 08:06:40 +0000

    add a function to the js file that opens the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,13 @@
 
 
 
+// Open And Close Modal Functions
+
+// Open Modal Function
+
+document.getElementById("open-modal").addEventListener("click", function () {
+    modal = document.getElementById("modal");
+    modal.style.display = "block";
+    modalOverlay = document.getElementById("overlay");
+    modalOverlay.style.opacity = 0.5;
+});
