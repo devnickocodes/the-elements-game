4ff77105ac20f1c81545738b74214c4af98d3ca9
devnickocodes <>
2023-11-30 07:20:03 +0000

    adjust the announceWinner function so that it resets the score once a winner is announced

--- assets/js/script.js
+++ assets/js/script.js
@@ -181,9 +181,18 @@ function announceWinner() {
         console.log("SAME");
     } else if (computerScoreSpan === 10) {
         announceLoss();
+        playerScoreSpan = document.getElementById("user-score");
+        computerScoreSpan = document.getElementById("computer-score");
+        playerScoreSpan.innerHTML = "0";
+        computerScoreSpan.innerHTML = "0";
         console.log("COMPUTER: TEN");
     } else if (playerScoreSpan === 10) {
         announceWin();
+        playerScoreSpan = document.getElementById("user-score");
+        computerScoreSpan = document.getElementById("computer-score");
+        playerScoreSpan.innerHTML = "0";
+        computerScoreSpan.innerHTML = "0";
+
     } else {
         console.log(playerScoreSpan + " " + computerScoreSpan);
     }
